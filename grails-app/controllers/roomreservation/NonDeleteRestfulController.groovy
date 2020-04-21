8d867228652db1e36352e1725732b59488cbc761
Benoit De Sousa <benoit.desousa@students.fhnw.ch>
2020-04-21 11:09:29 +0200

    commit

--- grails-app/controllers/roomreservation/NonDeleteRestfulController.groovy
+++ grails-app/controllers/roomreservation/NonDeleteRestfulController.groovy
@@ -1,16 +1,28 @@
 package roomreservation
 
 import grails.rest.RestfulController
+
 import static org.springframework.http.HttpStatus.METHOD_NOT_ALLOWED
 
-class NonDeleteRestfulController<T> extends RestfulController<T> {
+/**
+ * Custom RestfulController where we disable the delete action.
+ */
+abstract class NonDeleteRestfulController<T> extends RestfulController<T> {
 
-    NonDeleteRestfulController(Class<T> domainClass){ super (domainClass)}
+    // We need to provide the constructors, so the
+    // Resource transformation works.
+    NonDeleteRestfulController(Class<T> domainClass) {
+        this(domainClass, false)
+    }
 
-    NonDeleteRestfulController(Class<T> domainClass, boolean readOnly){ super (domainClass, readOnly)}
+    NonDeleteRestfulController(Class<T> domainClass, boolean readOnly) {
+        super(domainClass, readOnly)
+    }
 
     @Override
-    Object delete(){
+    def delete() {
+        // Return Method not allowed HTTP status code.
         render status: METHOD_NOT_ALLOWED
     }
+
 }
